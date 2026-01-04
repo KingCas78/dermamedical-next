8d514c1d10e17dce2659c92fa23fee709bbb65dc
César Castillo Salas <ccs@Cesars-MacBook-Pro.local>
2026-01-03 18:45:59 -0600

    feat: autoplay y loop infinito en ProductSlider

--- components/ProductSlider.tsx
+++ components/ProductSlider.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Swiper, SwiperSlide } from "swiper/react"
-import { Navigation, Pagination } from "swiper/modules"
+import { Navigation, Pagination, Autoplay } from "swiper/modules"
 
 import "swiper/css"
 import "swiper/css/navigation"
@@ -18,12 +18,17 @@ export default function ProductSlider() {
       <h2>Productos destacados</h2>
 
       <Swiper
-        modules={[Navigation, Pagination]}
+        modules={[Navigation, Pagination, Autoplay]}
         spaceBetween={24}
         navigation
         pagination={{ clickable: true }}
         slidesPerView={1.2}
         loop={true}   // 👈 habilita el loop infinito
+        autoplay={{
+          delay: 3000,                // tiempo entre cada slide (3 segundos)
+          disableOnInteraction: false, // sigue el autoplay aunque el usuario interactúe
+          pauseOnMouseEnter: true,     // 👈 se pausa al pasar el mouse encima
+        }}
         breakpoints={{
           640: { slidesPerView: 2 },
           1024: { slidesPerView: 3 },

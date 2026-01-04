4b63b26297ad8df3f0c9e29476a38e7bb077807c
César Castillo Salas <ccs@Cesars-MacBook-Pro.local>
2026-01-04 15:15:29 -0600

    Add slider to Home

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header className="header">
@@ -6,11 +8,11 @@ export default function Header() {
           <h1>Dra. Erika Ramírez</h1>
           <span>Dermamédical</span>
         </div>
-
+{/*navegación*/}
         <nav className="nav">
           <a href="#inicio">Inicio</a>
           <a href="#productos">Medicamentos</a>
-          <a  href="#HeroServices">Servicios</a>
+          <a  href="#servicios">Servicios</a>
           <a href="#nosotros">Nosotros</a>
           <a href="#contacto">Contacto</a>
           <a href="#blog">Blog</a>
